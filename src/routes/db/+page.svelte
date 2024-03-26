9bb82888132e931ac5115091a719912f52d12d0b
Lee-Seungje <moondgod@gmail.com>
2024-03-26 23:05:05 +0900

    Update ts parser

---
+++ src/routes/db/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import type { PageData } from './$types'
+
+  export let data: PageData
+  $: ({ tutorials } = data)
+
+  data.tutorials.forEach((item: any) =>
+    console.log('>>>> ' + item.title)
+  )
+</script>
