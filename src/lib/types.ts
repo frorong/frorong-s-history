536587dc5ee638b6335357cb5b82883c73a4f94f
Lee-Seungje <moondgod@gmail.com>
2024-03-27 22:43:32 +0900

    Add historyType

---
+++ src/lib/types.ts
@@ -0,0 +1,14 @@
+export type Status =
+  | 'PENDING'
+  | 'DOCUMENTS_PASSED'
+  | 'DOCUMENTS_FAILED'
+  | 'INTERVIEW_PASSED'
+  | 'FINALLY_PASSED'
+
+export interface HistoryType {
+  companyName: string
+  description?: string
+  date: Date
+  id: number
+  status: Status
+}
