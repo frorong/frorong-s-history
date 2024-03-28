97a2150d12b935ca64c6872a8d8b09d9a70d6266
Lee-Seungje <moondgod@gmail.com>
2024-03-28 19:56:24 +0900

    Resolve build error

--- src/routes/about/+page.ts
+++ src/routes/about/+page.ts
@@ -1,27 +1,15 @@
 import { error } from '@sveltejs/kit'
 
-interface Params {
-  slug: string
-}
-
-export async function load({
-  params,
-}: {
-  params: Params
-}) {
+export async function load() {
   try {
     const about = await import(
-      '../../constant/about/about.md'
+      `../../constant/about/about.md`
     )
 
     return {
       content: about.default,
-      meta: about.metadata,
     }
   } catch (e) {
-    throw error(
-      404,
-      `Could not find ${params.slug}`
-    )
+    throw error(404, `Could not find `)
   }
 }
