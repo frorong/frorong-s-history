b8981700f3a570a0c9d221c7b826ab1091af97f3
Lee-Seungje <moondgod@gmail.com>
2024-03-28 23:01:24 +0900

    Update meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,22 @@
   import ScrollToTopButton from '@components/scrollToTopButton.svelte'
 </script>
 
+<svelte:head>
+  <title>Frorong's history</title>
+  <meta
+    property="og:type"
+    content="article"
+  />
+  <meta
+    property="og:title"
+    content="Frorong's history"
+  />
+  <meta
+    property="og:image"
+    content="%sveltekit.assets%/openGraph.png"
+  />
+</svelte:head>
+
 <div
   class="w-full p-4 md:p-6 lg:p-8 xl:p-12"
 >
