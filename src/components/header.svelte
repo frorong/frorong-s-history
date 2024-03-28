5ce6b58ff7c2791784d4b8365314e671379216c9
Lee-Seungje <moondgod@gmail.com>
2024-03-28 18:15:29 +0900

    Update header

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,16 +1,36 @@
 <script lang="ts">
+  import { page } from '$app/stores'
+
+  let path = ''
+
+  page.subscribe(($page) => {
+    path = $page.url.pathname
+  })
 </script>
 
 <nav
   class="flex justify-start space-x-8 sm:ml-28"
 >
-  <a href="/" class="text-xl">
+  <a
+    href="/"
+    class="{`${path === '/' && 'font-bold text-blue-700'} text-xl`}"
+  >
     <b>Home</b>
   </a>
-  <a href="/about" class="text-xl"
+  <a
+    href="/about"
+    class="{`${path === '/about' && 'font-bold text-blue-700'} text-xl`}"
     >About</a
   >
-  <a href="/blog" class="text-xl"
+  <a
+    href="/blog"
+    class="{`${path === '/blog' && 'font-bold text-blue-700'} text-xl`}"
     >Blog</a
   >
 </nav>
+
+<style>
+  .text-blue-700 {
+    color: #3b82f6;
+  }
+</style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,7 +6,7 @@
   class="mt-8 flex w-full flex-col items-center space-y-8 pt-2"
 >
   <article
-    class="container flex flex-col items-center space-y-10 pt-10"
+    class="container flex flex-col items-center space-y-10"
   >
     <div
       class="markdown-container [&>h1]:my-12 [&>h1]:text-3xl [&>h1]:font-bold [&>h2]:my-6 [&>h2]:text-2xl [&>h2]:font-normal [&>h3]:my-6 [&>h3]:text-xl [&>h3]:font-bold [&>img]:mx-auto [&>img]:my-8 [&>img]:max-w-2xl"
@@ -21,7 +21,7 @@
 <style>
   .markdown-container {
     border: 0;
-    padding: 1rem 7rem;
+    padding: 0 7rem;
     font-size: 1rem;
     font-weight: 200;
     line-height: 1.5;
