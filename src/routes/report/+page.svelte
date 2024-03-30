a37ea0f04fa7b24d8d40b18604b51a5f4a24fe27
Lee-Seungje <moondgod@gmail.com>
2024-03-30 21:35:05 +0900

    Update html

--- src/routes/report/+page.svelte
+++ src/routes/report/+page.svelte
@@ -35,63 +35,40 @@
 
 <section>
   <h1
-    class="mb-4 text-2xl font-semibold capitalize"
+    class="my-4 text-2xl font-semibold capitalize"
   >
     문의하기
   </h1>
-
-  <form method="POST">
-    <input
-      name="subject"
-      type="text"
-      class="mb-4 w-full rounded-md border p-2"
-      placeholder="제목을 입력해주세요."
-      bind:value="{title}"
-      on:input="{handleTitleInput}"
-    />
-    <textarea
-      name="body"
-      class="w-full rounded-md border p-2"
-      rows="5"
-      placeholder="문의 내용을 입력해주세요."
-      bind:value="{message}"
-      on:input="{handleMessageInput}"
-    ></textarea>
-    <button
-      class="mt-2 rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
-      on:click="{handleSubmit}"
-      type="submit"
-    >
-      전송
-    </button>
-  </form>
 
   {#if $page.data.session}
     <div
       class="flex items-center space-x-4"
     >
-      {#if $page.data.session.user?.image}
-        <img
-          src="{$page.data.session.user
-            .image}"
-          alt="프로필 이미지"
-          class="h-12 w-12 rounded-full"
+      <form method="POST">
+        <input
+          name="subject"
+          type="text"
+          class="mb-4 w-full rounded-md border p-2"
+          placeholder="제목을 입력해주세요."
+          bind:value="{title}"
+          on:input="{handleTitleInput}"
         />
-      {/if}
-      <div>
-        <h1
-          class="text-xl font-semibold"
+        <textarea
+          name="body"
+          class="w-full rounded-md border p-2"
+          rows="5"
+          placeholder="문의 내용을 입력해주세요."
+          bind:value="{message}"
+          on:input="{handleMessageInput}"
+        ></textarea>
+        <button
+          class="mt-2 rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+          on:click="{handleSubmit}"
+          type="submit"
         >
-          {$page.data.session.user
-            ?.name ?? '익명의 유저'}님
-          안녕하세요!
-        </h1>
-        <p class="text-gray-600">
-          email: {$page.data.session
-            .user?.email ??
-            'email이 존재하지 않습니다.'}
-        </p>
-      </div>
+          전송
+        </button>
+      </form>
     </div>
   {:else}
     <div
