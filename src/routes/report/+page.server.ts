127f41a27a7297153bd669d31a5d46c43e8f2168
Lee-Seungje <moondgod@gmail.com>
2024-03-30 21:33:56 +0900

    Add server

--- src/routes/report/+page.server.ts
+++ src/routes/report/+page.server.ts
@@ -1,5 +1,8 @@
-// import { redirect } from '@sveltejs/kit'
-// import type { PageServerLoad } from './$types'
+import { redirect } from '@sveltejs/kit'
+import type { PageServerLoad } from './$types'
+
+import { GOOGLE_EMAIL } from '$env/static/private'
+import transporter from '$lib/emailSetup.server.js'
 
 // export const load: PageServerLoad =
 //   async (event) => {
@@ -9,3 +12,64 @@
 //       throw redirect(303, '/auth')
 //     return {}
 //   }
+
+export const actions = {
+  default: async ({
+    request,
+  }: {
+    request: any
+  }) => {
+    try {
+      const formData =
+        await request.formData()
+      const subject =
+        formData.get('subject')
+      const body = formData.get('body')
+      // eslint-disable-next-line no-console
+      console.log(body)
+      let html = `<h2>문의가 도착했습니다.</h2><pre>${body}</pre>`
+
+      const message = {
+        from: GOOGLE_EMAIL,
+        to: 'frorong0727@gmail.com',
+        bcc: 'https://www.frorong.shop',
+        subject: subject,
+        text: body,
+        html: html,
+      }
+
+      const sendEmail = async (
+        message: any
+      ) => {
+        await new Promise(
+          (resolve, reject) => {
+            transporter.sendMail(
+              message,
+              (
+                err: any,
+                info: unknown
+              ) => {
+                if (err) {
+                  // eslint-disable-next-line no-console
+                  console.error(err)
+                  reject(err)
+                } else {
+                  resolve(info)
+                }
+              }
+            )
+          }
+        )
+      }
+
+      await sendEmail(message)
+
+      return {
+        success: 'Email is sent',
+      }
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error)
+    }
+  },
+}
