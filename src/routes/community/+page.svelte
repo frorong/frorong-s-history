70ec3bed7899ba4b480196c209d9e17b21f31f39
Lee-Seungje <moondgod@gmail.com>
2024-03-30 20:31:29 +0900

    Add community

---
+++ src/routes/community/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  //   export let data
+</script>
+
+<section>
+  <h1
+    class="text-2xl font-semibold capitalize"
+  >
+    커뮤니티
+  </h1>
+  <p
+    class="text-xl font-semibold capitalize"
+  >
+    커뮤니티에서 다양한 이야기를
+    나눠보세요!
+  </p>
+</section>
