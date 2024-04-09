cbdca85e7f1a8461c138f7927f40c4253e49a243
Lee-Seungje <moondgod@gmail.com>
2024-04-09 15:56:12 +0900

    Add category

--- src/lib/types/blog.ts
+++ src/lib/types/blog.ts
@@ -1,7 +1,18 @@
+export type CategoryType =
+  | '전체'
+  | 'CS'
+  | 'JS'
+  | 'React'
+  | 'Svelte'
+  | 'TS'
+  | 'FE'
+  | 'NEXT'
+
 export interface BlogType {
   id: number
   title: string
   description: string
   date: string
   slug: string
+  category: CategoryType
 }
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import BlogCard from '@components/blogCard.svelte'
   export let data
+
+  export let selectedOption = '전체'
+  export const ALL_CATEGORY = '전체'
 </script>
 
 <section>
@@ -12,11 +15,28 @@
     >
       Frorong's blog
     </h1>
+
+    <select
+      class="mt-4 block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-800 shadow-sm focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50"
+      bind:value="{selectedOption}"
+    >
+      <option value="전체">전체</option>
+      <option value="CS">CS</option>
+      <option value="JS">JS</option>
+      <option value="React"
+        >React</option
+      >
+      <option value="Svelte"
+        >Svelte</option
+      >
+      <option value="TS">TS</option>
+      <option value="FE">FE</option>
+      <option value="Next">Next</option>
+    </select>
     <div
       class="mt-8 flex flex-col space-y-10"
     >
-      <span></span>
-      {#each data.blogs as blog (blog.id)}
+      {#each data.blogs.filter((blog) => blog.category == selectedOption || selectedOption == ALL_CATEGORY) as blog (blog.id)}
         <BlogCard blog="{blog}" />
       {/each}
     </div>
